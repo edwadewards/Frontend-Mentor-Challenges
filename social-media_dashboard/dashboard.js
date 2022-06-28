a4c38fba318ffdf889483b39e6fa7992b8f07ac6
Wade Edwards <wadecedwards@gmail.com>
2022-06-28 16:31:00 -0500

    Add files via upload

---
+++ social-media_dashboard/dashboard.js
@@ -0,0 +1,26 @@
+let darkMode = localStorage.getItem('darkMode');
+const darkModeToggle = document.querySelector('#checkbox');
+
+const enableDarkMode = () => {
+  document.body.classList.add('darkmode');
+  localStorage.setItem('darkMode', 'enabled');
+}
+
+const disableDarkMode = () => {
+  document.body.classList.remove('darkmode');
+  // localStorage.setItem('darkMode', null);
+  localStorage.removeItem('darkMode');
+}
+
+if (darkMode === 'enabled') {
+  enableDarkMode();
+}
+
+darkModeToggle.addEventListener('click', () => {
+  darkMode = localStorage.getItem('darkMode');
+  if (darkMode !== 'enabled') {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  };
+});
